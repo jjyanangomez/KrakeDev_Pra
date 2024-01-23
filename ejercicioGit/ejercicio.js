57fdc40309b2ddf3ab4cb4ceccb18e379442e2b4
jjyanangomez <juanyasa_04@hotmail.com>
2024-01-23 12:36:56 -0500

    el boton cambia de imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,7 @@
 saludar = function(){
+    let cmpImagensaludar;
+    cmpImagensaludar = document.getElementById("ImagenSaludar");
+    cmpImagensaludar.src="./imagenes/gif2.gif"
     console.log("ingresr a saludar");
+
 }
