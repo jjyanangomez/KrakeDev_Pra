27b59e3fef192a7a010efd5dffdc3331c0224dd8
jjyanangomez <juanyasa_04@hotmail.com>
2024-01-24 10:54:04 -0500

    Creación de funciones en servicioPuntoDeVenta y presentar el subtotal completado

--- utilitarios/utilitarios.js
+++ utilitarios/utilitarios.js
@@ -1,38 +1,38 @@
 
-mostrarTexto = function(idComponente,mensaje){
+mostrarImagen=function(idComponente,rutaImagen){
     let componente;
-    componente = document.getElementById(idComponente);
+    componente=document.getElementById(idComponente);
+    componente.src =  rutaImagen;
+}
+mostrarTexto=function(idComponente,mensaje){
+    let componente;
+    componente=document.getElementById(idComponente);
     componente.innerText = mensaje;
 }
-
-mostrarImagen = function(idComponente,rutaImagen){
-    let componente;
-    componente = document.getElementById(idComponente);
-    componente.src = rutaImagen;
-}
-
 mostrarTextoEnCaja = function(idComponente,mensaje){
     let componente;
-    componente = document.getElementById(idComponente);
+    componente=document.getElementById(idComponente);
     componente.value = mensaje;
 }
 
 recuperarTexto=function(idComponente){
     let componente;
     let valorIngresado;
-    componente = document.getElementById(idComponente);
-    valorIngresado = componente.value;
+    componente=document.getElementById(idComponente);
+    valorIngresado=componente.value;
     return valorIngresado;
 }
 
-recuperarInt=function(idComponente){
-    let valorCaja = recuperarTexto(idComponente);
-    let valorEntero = parseInt(valorCaja);
-    return valorEntero;
+recuperarInt = function(idComponente){
+   let valorCaja= recuperarTexto(idComponente);
+   let valorEntero = parseInt(valorCaja);
+   return valorEntero;
 }
 
-recuperarFloat=function(idComponente){
-    let valorCaja = recuperarTexto(idComponente);
-    let valorFloat = parseFloat(valorCaja);
-    return valorFloat;
-}
+recuperarFloat = function(idComponente){
+    let valorCaja= recuperarTexto(idComponente);
+    let valorFlotante = parseFloat(valorCaja);
+    return valorFlotante;
+ }
+
+
