ec032ca4839a58d43b935b7b840bb44d4fce6e94
jjyanangomez <juanyasa_04@hotmail.com>
2024-01-24 20:09:41 -0500

    Condicional para validar si el promedio es mayor a siete, completado

--- PromedioNotas/promedioNotas.js
+++ PromedioNotas/promedioNotas.js
@@ -2,7 +2,13 @@ calcularPromedioNotas=function(){
     let valorNota1 = recuperarFloat("txtNota1");
     let valorNota2 = recuperarFloat("txtNota2");
     let valorNota3 = recuperarFloat("txtNota3");
-
     let promedio = calcularPromedio(valorNota1,valorNota2,valorNota3);
+    let valorDosDecimales = promedio.toFixed(2);
+
+    if(valorDosDecimales>7){
+        mostrarImagen("imgResultado","./imagenes/exito.gif");
+    }else{
+        mostrarImagen("imgResultado","./imagenes/fracaso.gif");
+    }
     console.log(promedio.toFixed(2));
 }
