d047388106debb4201f82e309827a02dd48e9abe
jjyanangomez <juanyasa_04@hotmail.com>
2024-01-25 20:26:35 -0500

    Nueva funcionalidad para validar el promedio y presentar un mensaje con un gif, completado

--- PromedioNotas/promedioNotas.js
+++ PromedioNotas/promedioNotas.js
@@ -4,11 +4,24 @@ calcularPromedioNotas=function(){
     let valorNota3 = recuperarFloat("txtNota3");
     let promedio = calcularPromedio(valorNota1,valorNota2,valorNota3);
     let valorDosDecimales = promedio.toFixed(2);
+    if(valorDosDecimales > 0 && valorDosDecimales < 5){
+        mostrarTexto("lblMensaje","REPROBADO "+valorDosDecimales);
+        mostrarImagen("imgResultado","./imagenes/reprobado.gif");
+    }else if(valorDosDecimales>=5 && valorDosDecimales<=8){
+        mostrarImagen("imgResultado","./imagenes/buen.gif");
+        mostrarTexto("lblMensaje","BUEN TRABAJO: "+valorDosDecimales);
+    }else if(valorDosDecimales>8 && valorDosDecimales<=10){
+        mostrarImagen("imgResultado","./imagenes/exito.gif");
+        mostrarTexto("lblMensaje","EXCELENTE: "+valorDosDecimales);
+    }else{
+        mostrarTexto("lblMensaje","DATOS INCORRECTOS");
+        mostrarImagen("imgResultado","./imagenes/no.gif");
+    }
 
-    if(valorDosDecimales>7){
+    /*if(valorDosDecimales>7){
         mostrarImagen("imgResultado","./imagenes/exito.gif");
     }else{
         mostrarImagen("imgResultado","./imagenes/fracaso.gif");
-    }
+    }*/
     console.log(promedio.toFixed(2));
 }
