5c1960431a82ee415b6bda6505e31d7c879d9f87
jjyanangomez <juanyasa_04@hotmail.com>
2024-01-29 12:40:25 -0500

    Paso 6 completado del juego el ahorcado

--- juegoAhorcado/ahorcado.js
+++ juegoAhorcado/ahorcado.js
@@ -56,6 +56,8 @@ validar = function(letra){
     }
     if(coincidenciasAux == coincidencias){
         alert("LA LETRA NO ES PARTE DE LA PALABRA");
+        errores++;
+        mostrarAhorcado();
     }
 }
 ingresarLetra = function(){
@@ -72,4 +74,25 @@ ingresarLetra = function(){
         alert("SOLO SE ACEPTAN MAYUSCULAS");
     }
     intentos++;
+}
+mostrarAhorcado = function(){
+    if(errores == 1){
+        mostrarImagen("ahorcadoImagen","Ahorcado_01.png");
+    }else if(errores == 2){
+        mostrarImagen("ahorcadoImagen","Ahorcado_02.png");
+    }else if(errores == 3){
+        mostrarImagen("ahorcadoImagen","Ahorcado_03.png");
+    }else if(errores == 4){
+        mostrarImagen("ahorcadoImagen","Ahorcado_04.png");
+    }else if(errores == 5){
+        mostrarImagen("ahorcadoImagen","Ahorcado_05.png");
+    }else if(errores == 6){
+        mostrarImagen("ahorcadoImagen","Ahorcado_06.png");
+    }else if(errores == 7){
+        mostrarImagen("ahorcadoImagen","Ahorcado_07.png");
+    }else if(errores == 8){
+        mostrarImagen("ahorcadoImagen","Ahorcado_08.png");
+    }else if(errores == 9){
+        mostrarImagen("ahorcadoImagen","Ahorcado_09.png");
+    }
 }
