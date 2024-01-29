6c55258318a306d7e8f4358af2c40545b23b812d
jjyanangomez <juanyasa_04@hotmail.com>
2024-01-29 11:51:34 -0500

    Paso 2 del juego el ahorcado completado

--- juegoAhorcado/ahorcado.js
+++ juegoAhorcado/ahorcado.js
@@ -19,3 +19,16 @@ guardarPalabra = function(){
         }
     }
 }
+mostrarLetra = function(letra, posicion){
+    if(posicion==0){
+        mostrarTexto("div0",letra);
+    }else if(posicion==1){
+        mostrarTexto("div1",letra);
+    }else if(posicion==2){
+        mostrarTexto("div2",letra);
+    }else if(posicion==3){
+        mostrarTexto("div3",letra);
+    }else if(posicion==4){
+        mostrarTexto("div4",letra);
+    }
+}
