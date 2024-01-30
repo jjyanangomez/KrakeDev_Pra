2e165d407d5f200863c5c51c59925a6f0c0fa071
jjyanangomez <juanyasa_04@hotmail.com>
2024-01-29 19:27:49 -0500

    Creacion de tablas por medio de incercion html desde javascript

--- arreglos/ejercicio1.js
+++ arreglos/ejercicio1.js
@@ -18,6 +18,7 @@ probarAgregar = function(){
 }
 agregarNota = function(nota){
     notas.push(nota);
+    mostrarNotas();
 }
 calcularPromedio = function(){
     let sumaNotas = 0;
@@ -31,4 +32,26 @@ calcularPromedio = function(){
 ejecutarPromedio = function(){
     let resultado = calcularPromedio();
     mostrarTexto("lblPromedio",resultado);
+}
+
+// para generar tablas
+generarTabla = function(){
+    let contenidoTabla = "";
+    let cmpTabla = document.getElementById("divTabla");
+    contenidoTabla+="<table><tr><td>UNO</td></tr>"+
+                    "<tr><td>DOS</td></tr></table>"
+    cmpTabla.innerHTML = contenidoTabla;
+}
+mostrarNotas = function(){
+    let contenidoTabla = "<table><tr><th>NOTA</th></tr>";
+    let cmpTabla = document.getElementById("divTabla");
+    let miNota;
+    for(let i=0;i<notas.length;i++){
+        miNota = notas[i];
+        contenidoTabla+= "<tr><td>";
+        contenidoTabla+=miNota;
+        contenidoTabla+="</tr></td>"
+    }
+    contenidoTabla+="</table>"
+    cmpTabla.innerHTML =contenidoTabla;
 }
