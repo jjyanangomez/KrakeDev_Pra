4d1f1365d6bc807eb285bbfd8617715409523bd5
jjyanangomez <juanyasa_04@hotmail.com>
2024-01-29 19:51:00 -0500

    generarAleatorios completado y funcioanando correctamente, presentando en pantalla

--- aleatorios/aleatorios.js
+++ aleatorios/aleatorios.js
@@ -1,5 +1,4 @@
-let aleatorios =[];
-obtemerAleatorio = function(){
+obtenerAleatorio = function(){
     let aleatorio;
     let numeroMultiplicado;
     let numeroEntero
@@ -11,16 +10,33 @@ obtemerAleatorio = function(){
     return valorAleatorio;
 }
 generarAleatorios = function(){
+    let aleatorios =[];
     let valorIngresado = recuperarInt("txtLimite");
     if(!isNaN(valorIngresado)){
         if(!(valorIngresado>5 && valorIngresado<=20)){
             alert("El numero ingresado debe estar en el rango de 5 y 20");
         }else{
-            for(let i=0;i<valorIngresado+1;i++){
+            for(let i=0;i<valorIngresado;i++){
                 console.log(i);
+                let valor = obtenerAleatorio();
+                aleatorios.push(valor);
             }
+            mostrarResultado(aleatorios);
         }
     }else{
         alert("Por favor ingrese un número");
     }
+}
+mostrarResultado = function(arregloNumeros){
+    let valores;
+    let cmpTabla = document.getElementById("divTabla")
+    let contenidoTabla = "<table><tr><th>VALORES</th></tr>";
+    for(let i = 0; i < arregloNumeros.length; i++){
+        valores = arregloNumeros[i];
+        contenidoTabla+= "<tr><td>";
+        contenidoTabla+=valores;
+        contenidoTabla+= "</td></tr>";
+    };
+    contenidoTabla+= "</table>";
+    cmpTabla.innerHTML = contenidoTabla;
 }
