9b188e4b3acd51d4b0291f9b2c9d3014c7cb066f
jjyanangomez <juanyasa_04@hotmail.com>
2024-01-30 12:14:25 -0500

    Segunda parte de modificacion de atributos de objetos

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -53,3 +53,38 @@ crearCliente = function(){
     cliente1.apellido = "Salcedo",
     cliente1.cedula ="123456789"
 }
+probarIncrementarSaldo = function(){
+    let cta ={
+        numero: "532462879",
+        saldo: 34.0
+    }
+    incrementarSaldo(cta,100);
+    console.log(cta.saldo);
+}
+incrementarSaldo = function(cuenta,monto){
+    cuenta.saldo+=monto;
+}
+
+probarDeterminarMayor = function(){
+    let per1 ={
+        nombre : "Pedro",
+        edad : 24,
+    }
+    let per2 ={
+        nombre : "Maria",
+        edad : 26,
+    }
+    let mayor = determinarMayor(per1,per2);
+    if(mayor!=null){
+        console.log("El mayor es: "+mayor.nombre);
+    }
+}
+determinarMayor = function(persona1,persona2){
+    if(persona1.edad>persona2.edad){
+        return persona1;
+    }else if(persona2.edad>persona1.edad){
+        return persona2;
+    }else{
+        return null;
+    }
+}
