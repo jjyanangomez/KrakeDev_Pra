779db14dd08e486f9df0f3d25320537715ae083a
jjyanangomez <juanyasa_04@hotmail.com>
2024-01-30 23:16:27 -0500

    Desactivar y activar cajas de texto y botones completado

--- roles/roles.js
+++ roles/roles.js
@@ -1,3 +1,4 @@
+let esNuevo = false;
 let empleados = [
     {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
     {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0},
@@ -8,6 +9,12 @@ mostrarOpcionEmpleado = function(){
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
     mostrarEmpleado();
+    //Desabilitar componentes
+    deshabilitarComponente("txtCedula");
+    deshabilitarComponente("txtNombre");
+    deshabilitarComponente("txtApellido");
+    deshabilitarComponente("txtSueldo");
+    deshabilitarComponente("btnGuardar");
 }
 mostrarOpcionRol = function(){
     mostrarComponente("divRol");
@@ -40,4 +47,12 @@ mostrarEmpleado = function(){
     }
     contenidoTabla +="</table>"
     cmpTabla.innerHTML=contenidoTabla;
+}
+ejecutarNuevo = function(){
+    habilitarComponente("txtCedula");
+    habilitarComponente("txtNombre");
+    habilitarComponente("txtApellido");
+    habilitarComponente("txtSueldo");
+    habilitarComponente("btnGuardar");
+    esNuevo = true;
 }
